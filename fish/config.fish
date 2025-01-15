8b0b1390e214b5d964e278db51dafd9050949f9c
Aukai Tirrell <aukai.tirrell@conceptarthouse.com>
2025-01-14 18:57:35 -0700

    udpate fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,11 @@ starship init fish | source
 
 pfetch
 
-export VOLTA_FEATURE_PNPM=1
 
 pyenv init - | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/aukaitirrell/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/aukaitirrell/Downloads/google-cloud-sdk/path.fish.inc'; end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+export VOLTA_FEATURE_PNPM=1
